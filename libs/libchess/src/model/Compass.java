6258353d5a99522f5fda0c14ce5c0f50b48d9501
Clemens Horn <clemens.horn@mni.thm.de>
2023-12-22 22:14:58 +0100

    feat(libchess): add lerf squares and compass

---
+++ libs/libchess/src/model/Compass.java
@@ -0,0 +1,14 @@
+package model;
+
+// LERF (Little Endian Rank-File Mapping Compass)
+public abstract class Compass {
+
+	public static byte NORTH = 8;
+	public static byte NORTH_EAST = 9;
+	public static byte EAST = 1;
+	public static byte SOUTH_EAST = -7;
+	public static byte SOUTH = -8;
+	public static byte SOUTH_WEST = -9;
+	public static byte WEST = -1;
+	public static byte NORTH_WEST = 7;
+}
