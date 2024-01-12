06df8b45bde41e3bd647c069d9f72b943f3edf5f
Clemens Horn <clemens.horn@mni.thm.de>
2024-01-12 19:46:00 +0100

    test(chess): simple pawn moves

--- libs/libchess/test/model/move/irreversible/pawn/PawnMoveTest.java
+++ libs/libchess/test/model/move/irreversible/pawn/PawnMoveTest.java
@@ -1,7 +1,48 @@
 package model.move.irreversible.pawn;
 
+import static model.board.Square.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import model.board.Board;
+import model.board.IBoard;
+import model.piece.IPiece;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class PawnMoveTest {}
+public class PawnMoveTest {
+
+	private IBoard board;
+
+	@BeforeEach
+	void init() {
+		board = new Board();
+	}
+
+	@Test
+	void shouldUnsetPawnBit() {
+		IPiece piece = board.getWhitePawn();
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			11111111
+			00000000""";
+		assertEquals(expected, piece.getBitboard().toString());
+		new SinglePawnPushMove(e2, e3, board, piece);
+		expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00001000
+			11110111
+			00000000""";
+		assertEquals(expected, piece.getBitboard().toString());
+	}
+}
--- libs/libchess/test/model/move/irreversible/pawn/SinglePawnPushMoveTest.java
+++ libs/libchess/test/model/move/irreversible/pawn/SinglePawnPushMoveTest.java
@@ -1,7 +1,55 @@
 package model.move.irreversible.pawn;
 
+import static model.board.Square.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import model.board.Board;
+import model.board.IBoard;
+import model.piece.IPiece;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SinglePawnPushMoveTest {}
+public class SinglePawnPushMoveTest {
+
+	private IBoard board;
+
+	@BeforeEach
+	void init() {
+		board = new Board();
+	}
+
+	@Test
+	void shouldSetPawnBit() {
+		IPiece piece = board.getWhitePawn();
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			11111111
+			00000000""";
+		assertEquals(expected, piece.getBitboard().toString());
+		new SinglePawnPushMove(e2, e3, board, piece);
+		expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00001000
+			11110111
+			00000000""";
+		assertEquals(expected, piece.getBitboard().toString());
+	}
+
+	@Test
+	void shouldPrint() {
+		IPiece piece = board.getWhitePawn();
+		SinglePawnPushMove move = new SinglePawnPushMove(e2, e3, board, piece);
+		assertEquals("e2e3", move.toString());
+	}
+}
