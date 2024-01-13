2780737f983d1bf40bf50ad90fdb5601bf14a823
Clemens Horn <clemens.horn@mni.thm.de>
2024-01-13 15:09:10 +0100

    test(libchess): directions

--- libs/libchess/src/model/piece/Directions.java
+++ libs/libchess/src/model/piece/Directions.java
@@ -15,101 +15,101 @@ public enum Directions {
 	South,
 	SouthWest;
 
-	public static IBitboard getDiagonalRays(IBitboard piece, IBoard board) {
+	public static IBitboard getDiagonalRays(IBitboard slider, IBoard board) {
 		return Bitboard.mergeMany(
 			new IBitboard[] {
-				getRay(piece, board, NorthEast),
-				getRay(piece, board, NorthWest),
-				getRay(piece, board, SouthWest),
-				getRay(piece, board, SouthEast),
+				getRay(slider, board, NorthEast),
+				getRay(slider, board, NorthWest),
+				getRay(slider, board, SouthWest),
+				getRay(slider, board, SouthEast),
 			}
 		);
 	}
 
-	public static IBitboard getHorizontalRays(IBitboard piece, IBoard board) {
+	public static IBitboard getHorizontalRays(IBitboard slider, IBoard board) {
 		return Bitboard.mergeMany(
-			new IBitboard[] { getRay(piece, board, East), getRay(piece, board, West) }
+			new IBitboard[] { getRay(slider, board, East), getRay(slider, board, West) }
 		);
 	}
 
-	public static IBitboard getVerticalRays(IBitboard piece, IBoard board) {
+	public static IBitboard getVerticalRays(IBitboard slider, IBoard board) {
 		return Bitboard.mergeMany(
-			new IBitboard[] { getRay(piece, board, North), getRay(piece, board, South) }
+			new IBitboard[] { getRay(slider, board, North), getRay(slider, board, South) }
 		);
 	}
 
 	private static IBitboard getRay(IBitboard piece, IBoard board, Directions direction) {
 		IBitboard ray = new Bitboard();
-		IBitboard shift = piece.copy();
-		while (Directions.canSlide(shift, board, direction)) {
-			shift = Directions.shift(shift, direction);
-			ray.merge(shift);
+		IBitboard slider = piece.copy();
+		while (canSlide(slider, board, direction)) {
+			slider = shift(slider, direction);
+			ray.merge(slider);
 		}
-		shift = Directions.shift(shift, direction);
-		if (isEnemyCollision(shift, board)) {
-			ray.merge(shift);
+		slider = shift(slider, direction);
+		if (isEnemyCollision(slider, board)) {
+			ray.merge(slider);
 		}
 		return ray;
 	}
 
-	private static IBitboard shift(IBitboard piece, Directions direction) {
+	private static IBitboard shift(IBitboard slider, Directions direction) {
 		return switch (direction) {
-			case West -> Bitboard.shiftWest(piece);
-			case NorthWest -> Bitboard.shiftNorthWest(piece);
-			case North -> Bitboard.shiftNorth(piece);
-			case NorthEast -> Bitboard.shiftNorthEast(piece);
-			case East -> Bitboard.shiftEast(piece);
-			case SouthEast -> Bitboard.shiftSouthEast(piece);
-			case South -> Bitboard.shiftSouth(piece);
-			case SouthWest -> Bitboard.shiftSouthWest(piece);
+			case West -> Bitboard.shiftWest(slider);
+			case NorthWest -> Bitboard.shiftNorthWest(slider);
+			case North -> Bitboard.shiftNorth(slider);
+			case NorthEast -> Bitboard.shiftNorthEast(slider);
+			case East -> Bitboard.shiftEast(slider);
+			case SouthEast -> Bitboard.shiftSouthEast(slider);
+			case South -> Bitboard.shiftSouth(slider);
+			case SouthWest -> Bitboard.shiftSouthWest(slider);
 		};
 	}
 
-	private static boolean canSlide(IBitboard piece, IBoard board, Directions direction) {
-		return !isEdge(piece, direction) && pathFree(shift(piece, direction), board);
+	private static boolean canSlide(IBitboard slider, IBoard board, Directions direction) {
+		return !isEdge(slider, direction) && pathFree(shift(slider, direction), board);
 	}
 
-	private static boolean isFriendlyCollision(IBitboard piece, IBoard board) {
-		return Bitboard.overlap(piece, board.getPieces(board.getFen().isWhite()));
+	private static boolean isEdge(IBitboard slider, Directions direction) {
+		return switch (direction) {
+			case West -> isWestEdge(slider);
+			case North -> isNorthEdge(slider);
+			case East -> isEastEdge(slider);
+			case South -> isSouthEdge(slider);
+			case NorthWest -> isEdge(slider, North) || isEdge(slider, West);
+			case NorthEast -> isEdge(slider, North) || isEdge(slider, East);
+			case SouthEast -> isEdge(slider, South) || isEdge(slider, East);
+			case SouthWest -> isEdge(slider, South) || isEdge(slider, West);
+		};
 	}
 
-	private static boolean isEnemyCollision(IBitboard piece, IBoard board) {
-		return Bitboard.overlap(piece, board.getPieces(!board.getFen().isWhite()));
-	}
-
-	private static boolean pathFree(IBitboard slided, IBoard board) {
-		if (isFriendlyCollision(slided, board)) {
+	private static boolean pathFree(IBitboard slider, IBoard board) {
+		if (isFriendlyCollision(slider, board)) {
 			return false;
 		}
-		return !isEnemyCollision(slided, board);
+		return !isEnemyCollision(slider, board);
 	}
 
-	private static boolean isEdge(IBitboard piece, Directions direction) {
-		return switch (direction) {
-			case West -> westEdge(piece);
-			case NorthWest -> isEdge(piece, North) || isEdge(piece, West);
-			case North -> northEdge(piece);
-			case NorthEast -> isEdge(piece, North) || isEdge(piece, East);
-			case East -> eastEdge(piece);
-			case SouthEast -> isEdge(piece, South) || isEdge(piece, East);
-			case South -> southEdge(piece);
-			case SouthWest -> isEdge(piece, South) || isEdge(piece, West);
-		};
+	private static boolean isFriendlyCollision(IBitboard slider, IBoard board) {
+		return Bitboard.overlap(slider, board.getPieces(board.getFen().isWhite()));
 	}
 
-	private static boolean westEdge(IBitboard piece) {
-		return Bitboard.overlap(piece, Board.firstFile);
+	private static boolean isEnemyCollision(IBitboard slider, IBoard board) {
+		return Bitboard.overlap(slider, board.getPieces(!board.getFen().isWhite()));
 	}
 
-	private static boolean eastEdge(IBitboard piece) {
-		return Bitboard.overlap(piece, Board.lastFile);
+	private static boolean isWestEdge(IBitboard slider) {
+		return Bitboard.overlap(slider, Board.firstFile);
 	}
 
-	private static boolean northEdge(IBitboard piece) {
-		return Bitboard.overlap(piece, Board.eighthRank);
+	private static boolean isEastEdge(IBitboard slider) {
+		return Bitboard.overlap(slider, Board.lastFile);
 	}
 
-	private static boolean southEdge(IBitboard piece) {
-		return Bitboard.overlap(piece, Board.firstRank);
+	private static boolean isNorthEdge(IBitboard slider) {
+		return Bitboard.overlap(slider, Board.eighthRank);
+	}
+
+	private static boolean isSouthEdge(IBitboard slider) {
+		return Bitboard.overlap(slider, Board.firstRank);
 	}
 }
--- libs/libchess/test/model/piece/DirectionsTest.java
+++ libs/libchess/test/model/piece/DirectionsTest.java
@@ -1,3 +1,544 @@
 package model.piece;
 
-public class DirectionsTest {}
+import static model.board.Square.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.bits.Bitboard;
+import model.bits.IBitboard;
+import model.board.Board;
+import model.board.IBoard;
+import model.board.Square;
+import model.fen.ForsythEdwardsNotation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DirectionsTest {
+
+	private IBoard board;
+	private IBitboard slider;
+
+	@BeforeEach
+	void setup() {
+		board = new Board(new ForsythEdwardsNotation("8/8/8/8/8/8/8/8 w - - 0 1"));
+		slider = Bitboard.getSingleBit(Square.getIndex(h8));
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromTopRightOnEmptyBoard() {
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000010
+			00000100
+			00001000
+			00010000
+			00100000
+			01000000
+			10000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromTopLeftOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(a8));
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			01000000
+			00100000
+			00010000
+			00001000
+			00000100
+			00000010
+			00000001""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromBottomLeftOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(a1));
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			00000001
+			00000010
+			00000100
+			00001000
+			00010000
+			00100000
+			01000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromBottomRightOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(h1));
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			10000000
+			01000000
+			00100000
+			00010000
+			00001000
+			00000100
+			00000010
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromCenterOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(e4));
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			10000000
+			01000001
+			00100010
+			00010100
+			00000000
+			00010100
+			00100010
+			01000001""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromCenterAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(e4));
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			01000001
+			00100010
+			00010100
+			00000000
+			00010100
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromTopRightAsWhite() {
+		board = new Board();
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000010
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromTopLeftAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(a8));
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			01000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromBottomLeftAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(a1));
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetDiagonalRaysFromBottomRightAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(h1));
+		IBitboard rays = Directions.getDiagonalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromTopRightOnEmptyBoard() {
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000001
+			00000001
+			00000001
+			00000001
+			00000001
+			00000001
+			00000001""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromTopLeftOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(a8));
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			10000000
+			10000000
+			10000000
+			10000000
+			10000000
+			10000000
+			10000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromBottomLeftOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(a1));
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			10000000
+			10000000
+			10000000
+			10000000
+			10000000
+			10000000
+			10000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromBottomRightOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(h1));
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00000001
+			00000001
+			00000001
+			00000001
+			00000001
+			00000001
+			00000001
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromCenterOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(e4));
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00001000
+			00001000
+			00001000
+			00001000
+			00000000
+			00001000
+			00001000
+			00001000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromCenterAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(e4));
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00001000
+			00001000
+			00001000
+			00000000
+			00001000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromTopRightAsWhite() {
+		board = new Board();
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000001
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromTopLeftAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(a8));
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			10000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromBottomLeftAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(a1));
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetVerticalRaysFromBottomRightAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(h1));
+		IBitboard rays = Directions.getVerticalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromTopRightOnEmptyBoard() {
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			11111110
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromTopLeftOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(a8));
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			01111111
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromBottomLeftOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(a1));
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			01111111""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromBottomRightOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(h1));
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			11111110""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromCenterOnEmptyBoard() {
+		slider = Bitboard.getSingleBit(Square.getIndex(e4));
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			11110111
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromCenterAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(e4));
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			11110111
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromTopRightAsWhite() {
+		board = new Board();
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			00000010
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromTopLeftAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(a8));
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			01000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromBottomLeftAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(a1));
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+
+	@Test
+	void shouldGetHorizontalRaysFromBottomRightAsWhite() {
+		board = new Board();
+		slider = Bitboard.getSingleBit(Square.getIndex(h1));
+		IBitboard rays = Directions.getHorizontalRays(slider, board);
+		String expected =
+			"""
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000
+			00000000""";
+		assertEquals(expected, rays.toString());
+	}
+}
