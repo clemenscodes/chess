75e73eb0141dcde414b45743fe8bddddf34affe0
Clemens Horn <clemens.horn@mni.thm.de>
2024-01-15 21:02:50 +0100

    test(libchess): reader

--- libs/libchess/test/model/reader/ReaderTest.java
+++ libs/libchess/test/model/reader/ReaderTest.java
@@ -12,9 +12,7 @@ public class ReaderTest {
 	@Test
 	void shouldReadLineCorrectly() {
 		String inputString = "Hello, World!";
-		InputStream inputStream = new ByteArrayInputStream(
-			inputString.getBytes(StandardCharsets.UTF_8)
-		);
+		InputStream inputStream = new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8));
 		Reader reader = new Reader(inputStream);
 		String result = reader.readLine();
 		assertEquals(inputString, result.trim());
@@ -23,11 +21,29 @@ public class ReaderTest {
 	@Test
 	void shouldReadEmptyLineCorrectly() {
 		String inputString = "";
-		InputStream inputStream = new ByteArrayInputStream(
-			inputString.getBytes(StandardCharsets.UTF_8)
-		);
+		InputStream inputStream = new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8));
 		Reader reader = new Reader(inputStream);
 		String result = reader.readLine();
 		assertEquals(inputString, result);
+	}
+
+	@Test
+	void shouldReadFromBufferWhenNotEmpty() {
+		String inputString = "First Line\nSecond Line";
+		InputStream inputStream = new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8));
+		Reader reader = new Reader(inputStream);
+		reader.readLine();
+		reader.readLine();
+		String result = reader.readLine();
+		assertEquals("Second Line", result.trim());
+	}
+
+	@Test
+	void shouldReadFromStreamWhenBufferEmpty() {
+		String inputString = "First Line\nSecond Line";
+		InputStream inputStream = new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8));
+		Reader reader = new Reader(inputStream);
+		String result = reader.readLine();
+		assertEquals("First Line", result.trim());
 	}
 }
