aa2ea9c3d3b9c64386fe92dca02b9dc29092f759
Clemens Horn <clemens.horn@mni.thm.de>
2024-01-23 12:20:31 +0100

    docs(chess): javadoc

--- apps/chess/src/chess/Chess.java
+++ apps/chess/src/chess/Chess.java
@@ -8,8 +8,19 @@ import model.ChessModel;
 import processing.core.PApplet;
 import view.ChessView;
 
+/**
+ * The {@link Chess} class serves as the main entry point for the chess game application,
+ * utilizing the API, model, view, and controller libraries to bootstrap a fully functioning chess game.
+ * It initializes the model, view, and controller components, establishing communication between them,
+ * and launches the game using the Processing library.
+ */
 public final class Chess {
 
+	/**
+	 * The main method that initializes and starts the chess game.
+	 *
+	 * @param args Command line arguments (not used).
+	 */
 	public static void main(String[] args) {
 		final int width = 1600;
 		final int height = 900;
